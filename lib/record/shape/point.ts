ce96ea1d8bee4adff7cd8daebe0a71d3af373efb
HyunnoH <43626722+HyunnoH@users.noreply.github.com>
2021-01-21 21:03:03 +0900

    Point 파싱 함수 오버로딩 추가

--- lib/record/shape/point.ts
+++ lib/record/shape/point.ts
@@ -1,10 +1,14 @@
-export function parsePoint(data: ArrayBuffer, offset: number) {
-  // console.log(data.byteLength, offset);
-  const view = new DataView(data, offset, 20);
+import { Point, ShapeType } from "../../types";
+
+export function parsePoint(data: ArrayBuffer, offset: number): Point | ShapeType;
+export function parsePoint(data: ArrayBuffer, offset: number, insideContents: false): Point | ShapeType
+export function parsePoint(data: ArrayBuffer, offset: number, insideContents: true): Point
+export function parsePoint(data: ArrayBuffer, offset: number, insideContents?: boolean): Point | (Point | ShapeType) {
+  const view = new DataView(data, offset, insideContents ? 16 : 20);
 
   return {
-    type: view.getInt32(16, true),
-    x: view.getFloat64(4, true),
-    y: view.getFloat64(12, true),
+    ...(!insideContents && {type: view.getInt32(0, true),}),
+    x: view.getFloat64(insideContents ? 0 : 4, true),
+    y: view.getFloat64(insideContents ? 8 : 12, true),
   };
 }
