7cb06a579355a02f47762ecb1d785ff4113e531d
HyunnoH <43626722+HyunnoH@users.noreply.github.com>
2021-01-21 21:03:36 +0900

    enum 이름 변경 및 point & polyline 파싱 추가

--- lib/record/index.ts
+++ lib/record/index.ts
@@ -1,7 +1,9 @@
 import { ShapeType } from "../types";
+import { parsePoint } from "./shape/point";
+import { parsePolyLine } from "./shape/poly-line";
 import { parsePolygon } from "./shape/polygon";
 
-enum RecordHeader {
+enum RecordHeaderIndex {
   RecordNumber = 0,
   ContentLength = 4,
 }
@@ -10,8 +12,8 @@ export function parseRecordHeader(data: ArrayBuffer, offset: number) {
   const view = new DataView(data, offset, 8);
 
   return {
-    recordNumber: view.getInt32(RecordHeader.RecordNumber, false),
-    contentLength: view.getInt32(RecordHeader.ContentLength, false),
+    recordNumber: view.getInt32(RecordHeaderIndex.RecordNumber, false),
+    contentLength: view.getInt32(RecordHeaderIndex.ContentLength, false),
   };
 }
 
@@ -24,6 +26,10 @@ export function parseRecordContents(
   const type: ShapeType = view.getInt32(0, true);
 
   switch (type) {
+    case ShapeType.Point:
+      return parsePoint(data, startingPoint)
+    case ShapeType.PolyLine:
+      return parsePolyLine(data, startingPoint, contentLength)
     case ShapeType.Polygon:
       return parsePolygon(data, startingPoint, contentLength);
     default:
